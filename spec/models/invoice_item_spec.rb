1758e18453ddb3a954e45cc7d19b6b917a7b4c4b
PlanetEfficacy <jspevack@gmail.com>
2016-10-04 17:03:40 -0600

    add record endpoints for invoice items

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 RSpec.describe InvoiceItem, type: :model do
   it { should belong_to(:invoice) }
   it { should belong_to(:item) }
+
+  it "can return a random invoice item" do
+    create_list(:invoice_item, 2)
+    invoice_item = InvoiceItem.random
+    expect(invoice_item).to be_instance_of(InvoiceItem)
+  end
 end
