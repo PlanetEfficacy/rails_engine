c594d90cc8afee66a7defa6268028027d23e9a70
PlanetEfficacy <jspevack@gmail.com>
2016-10-05 10:42:50 -0600

    clean query

--- app/controllers/api/v1/merchants/customers_controller.rb
+++ app/controllers/api/v1/merchants/customers_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::Merchants::CustomersController < ApplicationController
   def index
-    customers = Merchant.find(params["id"]).customers
+    render json: Merchant.find(params["id"]).customers
                         .joins(:invoices)
                         .joins(:transactions)
                         .where(transactions: {result: "failed"})
-    render json: customers
   end
 end
