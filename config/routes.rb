61d2406819f353ad1dd3246d1536be68de58c8c1
PlanetEfficacy <jspevack@gmail.com>
2016-10-06 10:37:15 -0600

    add items to invoice_items relationship

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
         get 'random', to: 'random#show'
         get 'revenue', to: 'all_revenue#show'
         get 'most_revenue', to: 'all_revenue#index'
-        get ":id/customers_with_pending_invoices", to: "customers#index"
-        get ":id/items", to: "items#index"
-        get ":id/invoices", to: "invoices#index"
-        get ":id/favorite_customer", to: "customers#show"
+        get ':id/customers_with_pending_invoices', to: 'customers#index'
+        get ':id/items', to: 'items#index'
+        get ':id/invoices', to: 'invoices#index'
+        get ':id/favorite_customer', to: 'customers#show'
       end
 
       namespace :customers do
@@ -25,6 +25,7 @@ Rails.application.routes.draw do
         get 'find', to: 'search#show'
         get 'random', to: 'random#show'
         get 'most_revenue', to: 'revenue#index'
+        get ':id/invoice_items', to: 'invoice_items#show'
       end
 
       namespace :invoices do
@@ -42,8 +43,8 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'find', to: 'search#show'
         get 'random', to: 'random#show'
-        get ":id/invoice", to: "invoice#show"
-        get ":id/item", to: "item#show"
+        get ':id/invoice', to: 'invoice#show'
+        get ':id/item', to: 'item#show'
       end
 
       namespace :transactions do
---
+++ spec/requests/api/v1/relationship_items_invoice_items_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "get to items/:id/invoice_items" do
+  it "returns the associated invoice item" do
+    new_item = create(:item)
+    invoice_item = create_list(:invoice_item, 2, item_id: new_item.id)
+
+    get "/api/v1/items/#{new_item.id}/invoice_items"
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_items).to be_instance_of(Array)
+    expect(invoice_items.count).to eq(2)
+
+    invoice_items.each { |invoice_item| expect(invoice_item["item_id"]).to eq(new_item.id) }
+  end
+end
