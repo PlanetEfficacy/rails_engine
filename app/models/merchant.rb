6321f32362f3525a58e2209e43e7f2eb42c14f9f
Nate Anderson <curtislowe10@gmail.com>
2016-10-06 09:02:49 -0600

    fix lingering merge conflict

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,12 +1,10 @@
 class Merchant < ApplicationRecord
   has_many :invoices
   has_many :customers, through: :invoices
-<<<<<<< HEAD
   has_many :invoice_items, through: :invoices
   has_many :transactions, through: :invoices
-=======
   has_many :items
->>>>>>> master
+
 
   def self.random
     offset = rand(Merchant.count)
