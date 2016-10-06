83d23473b1db9dba5b2372428dc88cdcc2ec87e6
PlanetEfficacy <jspevack@gmail.com>
2016-10-06 12:54:50 -0600

    change merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -20,17 +20,6 @@ class Merchant < ApplicationRecord
   def customers_with_pending_invoices
     customers.joins("join transactions on transactions.invoice_id = invoices.id")
             .merge(Transaction.pending).distinct
-
-
-
-    # select distinct on (customers.id) customers.* from customers inner join invoices
-    # on invoices.customer_id = customers.id inner
-    # join transactions on transactions.invoice_id = invoices.id where
-    # invoices.merchant_id = 17 and transactions.result = 'failed'
-
-    # customers.joins(:invoices).joins(:transactions).where(transactions: )
-    # invoices.joins(:transactions).where(transactions: {result: "failed"}).joins(:customer).distinct
-    # customers.joins(:transactions).where(transactions: {result: "failed"})
   end
 
   private
