a9b3662e775bbc37b573bb688b73211e02d39858
PlanetEfficacy <jspevack@gmail.com>
2016-10-06 10:51:48 -0600

    pass transaction relationship spec harness

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,7 @@ Rails.application.routes.draw do
         get 'find_all', to: 'search#index'
         get 'find', to: 'search#show'
         get 'random', to: 'random#show'
+        get ':id/invoice', to: 'invoice#show'
       end
 
       resources :items, only: [:index, :show]
