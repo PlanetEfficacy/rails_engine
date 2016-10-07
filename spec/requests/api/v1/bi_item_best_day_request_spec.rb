a9b17bf77e2db6dd9e2950e6048fbd3aefa65d76
PlanetEfficacy <jspevack@gmail.com>
2016-10-06 20:19:24 -0600

    reconcile spec harness

---
+++ spec/requests/api/v1/bi_item_best_day_request_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "get request to single item's best day" do
+  xit "returns date (or dates in case of tie) with most sales for item by invoice date" do
+    # item_1 = create(:item)
+    # item_2 = create(:item)
+    # invoice = create(:invoice)
+    # invoice_item_1 = create(:invoice_item,
+    #                           item_id: item_1.id,
+    #                           invoice_id: invoice.id,
+    #                           quantity: 2,
+    #                           unit_price: 1000)
+    # invoice_item_2 = create(:invoice_item,
+    #                           item_id: item_2.id,
+    #                           invoice_id: invoice.id,
+    #                           quantity: 1,
+    #                           unit_price: 1000)
+    # create(:transaction, invoice_id: invoice.id, result: "success")
+    #
+    # get "/api/v1/items/most_revenue?quantity=2"
+    # items = JSON.parse(response.body)
+    #
+    # expect(response).to be_success
+    # expect(items).to be_instance_of(Array)
+    # expect(items.length).to eq(2)
+    # expect(items.first["id"]).to eq(item_1.id)
+    # expect(items.last["id"]).to eq(item_2.id)
+  end
+end
