580a9d1e622379cd2645a0707ee777491370e266
PlanetEfficacy <jspevack@gmail.com>
2016-10-08 15:30:25 -0600

    update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
-  include ActionController::Serialization
-
   def fix_unit_price(whitelist)
     if whitelist["unit_price"]
       whitelist["unit_price"] = whitelist["unit_price"].gsub('.', '')
